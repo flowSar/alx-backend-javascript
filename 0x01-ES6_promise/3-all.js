284806019a5f2573e0acc99cd24a994d0e86090f
flowsar <brahim.sar151@gmail.com>
2024-10-05 00:31:25 +0100

    format

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
@@ -7,6 +7,6 @@ export default function handleProfileSignup() {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      console.log('Signup system offline');
+      console.log("Signup system offline");
     });
-} 
+}
