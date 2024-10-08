9c206ded81a9e94c2e90e5a420dff39306566798
flowsar <brahim.sar151@gmail.com>
2024-10-08 23:32:43 +0100

    dataview

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,17 @@
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+
+  const int8View = new Int8Array(buffer);
+
+  const dataView = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
+
+  int8View[position] = value;
+
+  return dataView;
+};
+
+export default createInt8TypedArray;
