f3a837ebfcc09d914f7131b5affdf90d00a2e2aa
flowsar <brahim.sar151@gmail.com>
2024-10-08 21:40:02 +0100

    fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
-export default getListStudents = () => {
+const getListStudents = () => {
   return [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
     { id: 3, firstName: "Serena", location: "San Francisco" },
   ];
 };
+export default getListStudents;
