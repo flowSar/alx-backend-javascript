90e5dcb757e2e257831d040998b5464491ad62c5
flowsar <brahim.sar151@gmail.com>
2024-10-08 23:21:42 +0100

    filter and map combined

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+const updateStudentGradeByCity = (StudentList, city, newGrades) => {
+  return StudentList.filter((student) => student.location === city).map(
+    (student) => {
+      const studentGrade = newGrades.find(
+        (stu) => stu.studentId === student.id
+      );
+      if (studentGrade) {
+        student.grade = studentGrade.grade;
+      } else {
+        student.grade = "N/A";
+      }
+      return student;
+    }
+  );
+};
+
+export default updateStudentGradeByCity;
