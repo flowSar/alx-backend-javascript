14ba4213d331e6aac3408d93cd760de3a87bc711
flowsar <brahim.sar151@gmail.com>
2024-10-26 12:24:34 +0100

    coma

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,5 +1,7 @@
 import getNeighborhoodsList from "./2-arrow.js";
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood("Noe Valley");
-console.log(res);
+import getSumOfHoods from "./3-default-parameter.js";
+
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
-
-  const self = this;
-  this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
-  };
+export default function getSumOfHoods(
+  initialNumber,
+  expansion1989,
+  expansion2019
+) {
+  if (expansion1989 === undefined)
+    return (
+      initialNumber +
+      (expansion1989 === undefined ? 89 : 0) +
+      (expansion2019 === undefined ? 19 : 0)
+    );
 }
