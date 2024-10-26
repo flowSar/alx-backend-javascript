7cba91d6df6cbe96697fbcc54874e2540220e280
flowsar <brahim.sar151@gmail.com>
2024-10-26 14:07:07 +0100

    single qu

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-export default function getSumOfHoods(
-  initialNumber,
-  expansion1989,
-  expansion2019
-) {
-  if (expansion1989 === undefined)
-    return (
-      initialNumber +
-      (expansion1989 === undefined ? 89 : 0) +
-      (expansion2019 === undefined ? 19 : 0)
-    );
+function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: "$119,868",
+    gdp: "$154.2 billion",
+    capita: "$178,479",
+  };
+
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
