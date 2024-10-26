2263071cbc55ad14f2cc3e5c42a901d652715136
flowsar <brahim.sar151@gmail.com>
2024-10-26 11:37:11 +0100

    single qu

--- 0x00-ES6_basic/test.js
+++ 0x00-ES6_basic/test.js
@@ -1,5 +1,7 @@
+#!/usr/bin/node
+
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
