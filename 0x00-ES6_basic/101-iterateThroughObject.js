5c531af2bf4ae17038eacb9ea2421d2005083872
flowsar <brahim.sar151@gmail.com>
2025-01-29 13:55:26 +0100

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,1 +1,8 @@
- 
+export default function iterateThroughObject(reportWithIterator) {
+  const returnString = [];
+  for (const report of reportWithIterator) {
+    returnString.push(report);
+  }
+  return returnString.join(' | ');
+}
+
